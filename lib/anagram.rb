13cf3b1c617e97fc13e4d44491322ee958cf2da9
Audra Tallman <audi.tallman@gmail.com>
2019-03-24 18:46:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,12 @@ class Anagram
 
 attr_accessor :word
 
-def initialize(word)
-  @word= word
-end
+  def initialize(word)
+    @word= word
+  end
 
-def match(list)
-  list.select {|word| word.split("").sort == @word.split("").sort} 
-end 
+  def match(list)
+    list.select {|word| word.split("").sort == @word.split("").sort} 
+  end 
   
 end
