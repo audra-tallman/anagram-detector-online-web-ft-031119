831828d713644834d90756c54870c78e22754ae0
Audra Tallman <audi.tallman@gmail.com>
2019-03-24 18:41:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word= word
+  if word == list
+    puts list << word
+  else
+    puts []
+
+  end
+end 
+  
+end
